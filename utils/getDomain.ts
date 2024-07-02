77755f1326fea31f9400d75da42226f3789fb6d4
Arsalan Rabdanov <uzarsalan@gmail.com>
2024-07-02 16:16:18 +0800

    fixed vercel url

--- utils/getDomain.ts
+++ utils/getDomain.ts
@@ -1,5 +1,5 @@
 export function getDomain() {
-  if (process.env.VERCEL_URL) return process.env.VERCEL_URL;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   if (process.env.NEXT_PUBLIC_BASE_URL) return process.env.NEXT_PUBLIC_BASE_URL;
 
   return "http://localhost:3000";
