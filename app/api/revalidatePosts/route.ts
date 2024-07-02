2bab74493172a9215168e3f00a8cbafbde482df0
Arsalan Rabdanov <uzarsalan@gmail.com>
2024-07-02 17:23:09 +0800

    fixed route return type

--- app/api/revalidatePosts/route.ts
+++ app/api/revalidatePosts/route.ts
@@ -1,7 +1,7 @@
 import { revalidatePath } from "next/cache";
 
 export function GET(request: Request) {
-  if (request.headers.get("token") !== process.env.REVALIDATE_TOKEN)
-    return null;
+  if (request.headers.get("token") !== process.env.REVALIDATE_TOKEN) return;
+
   revalidatePath("/");
 }
