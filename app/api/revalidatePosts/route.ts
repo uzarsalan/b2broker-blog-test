11c26608969502fa8e7989efc62103bccab998de
Arsalan Rabdanov <uzarsalan@gmail.com>
2024-07-02 18:51:29 +0800

    revalidate all routes on trigger

--- app/api/revalidatePosts/route.ts
+++ app/api/revalidatePosts/route.ts
@@ -1,10 +1,10 @@
 import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
-export function GET(request: Request) {
+export async function GET(request: Request) {
   if (request.headers.get("token") !== process.env.REVALIDATE_TOKEN)
     return NextResponse.json({ error: "Invalid token" });
 
-  revalidatePath("/");
+  revalidatePath("/", "layout");
   return NextResponse.json({ ok: true });
 }
